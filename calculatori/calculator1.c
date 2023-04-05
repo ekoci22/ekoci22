91a11da265107ab239aa35938f985bf39c5cbdc3
erdi koci <erdikoci97@gmail.com>
2023-04-05 18:19:05 +0200

    area and calculator branch

--- calculatori/calculator1.c
+++ calculatori/calculator1.c
@@ -8,26 +8,70 @@ void division (char op);
 void power (char op);
 void sqrtFunction (char op);
 void average(char op);
+void squararea(char op);
+void squarperimeter(char op,double a);
+void circlearea(char op);
+void circleperimeter(char op,double pi,double r);
+
+
+void squararea(char op)
+{
+    double area;
+    double a;
+    printf("Put length a:");
+    scanf("%lf",&a);
+    area=a*a;
+    squarperimeter(op,a);
+    printf("Area: %.1lf\n",area);
+}
+void squarperimeter(char op,double a)
+{
+    double perimeter;
+    perimeter=a*4;
+    printf("Perimeter: %.1lf\n",perimeter);
+}
+void circlearea(char op)
+{
+    double pi=3.14159;
+    double area;
+    double r;
+    printf("Put radius:");
+    scanf("%lf", &r);
+    area=(double)pi*r*r;
+    circleperimeter( op, pi, r);
+    printf("Area: %.1lf \n",area);
+}
+void circleperimeter(char op,double pi,double r)
+{
+    double perimeter;
+    perimeter=(double )2*pi*r;
+    printf("Perimeter: %.1lf\n",perimeter);
+}
+
+
 
 void average (char op)
 {
     double sum=0,number;
     double avg;
     int n=0;
-    printf("Put number to find avg (put -999 to stop)");
-    while (1)
+    if(op=='a')
     {
-        scanf("%lf",&number);
-        if(number==-999)
-        break;
-        else
-          n++;
-          sum+=number;
+        printf("Put number to find avg (put -999 to stop)");
+        while (1)
+        {
+            scanf("%lf",&number);
+            if(number==-999)
+                break;
+            else
+                n++;
+            sum+=number;
 
-        
+
+        }
+        avg=(double )sum/n;
+        printf("Number average: %.1lf",avg);
     }
-    avg=(double )sum/n;
-    printf("Number average: %.1lf",avg);   
 }
 void add(char op)
 {
@@ -114,7 +158,24 @@ int main()
     division( op );
     power( op);
     sqrtFunction(op);
+    average(op);
 
+    if(op=='b')
+    {
+        char t;
+        printf("Put s for area and perimeter of square and c for area and perimeter of circle:  ");
+        scanf(" %c",&t);
 
+        if(t=='s')
+        {
+            squararea(op);
+
+        }
+        else if(t=='c')
+        {
+            circlearea(op);
+
+        }
+    }
     return 0;
 }
